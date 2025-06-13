5b89c6b34346ff341321ea986411bf7785d3817d
hitallof <hitalloufpe@gmail.com>
2025-06-13 11:20:15 -0300

    ajustado

--- internal/consultor/model.go
+++ internal/consultor/model.go
@@ -1,19 +1,22 @@
 package consultor
 
 import (
+	"api/internal/contrato" // ← importe aqui
 	"api/internal/negociacao"
+
 	"gorm.io/gorm"
 )
 
 type Consultor struct {
 	gorm.Model
-	Nome                  string                   `json:"nome"`
-	Sobrenome             string                   `json:"sobrenome"`
-	CNPJ                  string                   `json:"cnpj" gorm:"unique"`
-	Email                 string                   `json:"email" gorm:"unique"`
-	Telefone              string                   `json:"telefone"`
-	Foto                  string                   `json:"foto"`
-	Senha                 string                   `json:"-"`
-	PrecisaRedefinirSenha bool                     `json:"-"`
-	Negociacoes           []negociacao.Negociacao  `gorm:"foreignKey:ConsultorID" json:"negociacoes"`
+	Nome                  string                  `json:"nome"`
+	Sobrenome             string                  `json:"sobrenome"`
+	CNPJ                  string                  `json:"cnpj" gorm:"unique"`
+	Email                 string                  `json:"email" gorm:"unique"`
+	Telefone              string                  `json:"telefone"`
+	Foto                  string                  `json:"foto"`
+	Senha                 string                  `json:"-"`
+	PrecisaRedefinirSenha bool                    `json:"-"`
+	Negociacoes           []negociacao.Negociacao `gorm:"foreignKey:ConsultorID" json:"negociacoes"`
+	Contratos             []contrato.Contrato     `gorm:"foreignKey:ConsultorID" json:"contratos"` // ← adicione isto
 }
--- internal/contrato/model.go
+++ internal/contrato/model.go
@@ -2,14 +2,16 @@ package contrato
 
 import (
 	"time"
+
 	"gorm.io/gorm"
 )
 
 type Contrato struct {
 	gorm.Model
-	NegociacaoID    uint      `json:"negociacaoId"`                   // 🔗 FK para Negociacao
-	Valor           float64   `json:"valor"`                          // 💰 Valor do contrato
-	InicioSuprimento time.Time `json:"inicioSuprimento"`              // 📅 Data início
-	FimSuprimento    time.Time `json:"fimSuprimento"`                 // 📅 Data fim
-	ValorIntegral   bool      `json:"valorIntegral"`                  // ✅ Se o valor da comissão foi pago integralmente no início
+	NegociacaoID     uint      `json:"negociacaoId"`
+	ConsultorID      uint      `json:"consultorId"` // 🔗 FK para Consultor
+	Valor            float64   `json:"valor"`
+	InicioSuprimento time.Time `json:"inicioSuprimento"`
+	FimSuprimento    time.Time `json:"fimSuprimento"`
+	ValorIntegral    bool      `json:"valorIntegral"`
 }
